6416bbcc62eac651eebca60439d783a9eaa21f33
Aleksandr Mingalev <amingalev@gmail.com>
2023-09-08 03:13:34 +0200

    feat: implement all necessary methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.min;
+      this.max;
+      this.curr;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+       this.curr = Math.ceil((this.max-this.min) / 2 + this.min);  
+       return this.curr;
     }
 
     lower() {
-
+      this.max = this.curr;
     }
 
     greater() {
-
+      this.min = this.curr;
     }
 }
 
